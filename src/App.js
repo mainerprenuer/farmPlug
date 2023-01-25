f274a533afaa90e86853284dcf36c459fd8011cd
Mousty Mainer <moustymainer@gmail.com>
2023-01-25 15:31:22 +0100

    Initial commit, Setting up !

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+ReactDOM.render (
+    <App />, document.getElementById ("root")
+);
