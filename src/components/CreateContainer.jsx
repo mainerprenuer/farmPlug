49de2d936ffec61b0c939a9c98aa49f73a4312d4
Mousty Mainer <moustymainer@gmail.com>
2023-02-04 19:35:05 +0100

    working on createItems comps

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import { BiFoodMenu } from 'react-icons/bi';
+import { categories } from '../utils/data';
+import Loader from './Loader';
 
 const CreateContainer = () => {
 
@@ -13,11 +15,11 @@ const CreateContainer = () => {
   const [fields, setfields] = useState(false);
   const [alertStatus, setStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
-      <div className='w-[90%] md:w-[75%] border border-gray-400 rounded-lg p-4 flex flex-col items-center justify-center'>
+      <div className='w-[90%] md:w-[75%] border border-gray-400 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
          {
           fields && (
             <motion.p initial={{opacity : 0}}
@@ -39,9 +41,31 @@ const CreateContainer = () => {
            placeholder="Give me a title..."
            className='w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text text-textColor'></input>
          </div>
+
+         <div className='w-full'>
+          <select onChange={(e) => setcategory(e.target.value)} className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer">
+            <option value="other" className='bg-white'>Select Category</option>
+            {categories && categories.map(item => (
+              <option key={item.id} className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+              value={item.urlParamName}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+         </div>
+
+        <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg'>
+              {isLoading ? <Loader /> : <>
+                  {!imgAsset ? <>
+                    <label className='w-ful h-full '>
+
+                    </label>
+                  </> : <></>}
+              </>}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreateContainer
--- src/utils/data.js
+++ src/utils/data.js
@@ -10,4 +10,48 @@ export const heroData = [
     {id : 2, name : 'Fresh Banana', desc : 'Banana & Plantain', price : '1,000', imageSrc : F9},
     {id : 3, name : 'Watermelon', desc : 'Sweet watermelon', price : '300', imageSrc : F10},
     {id : 4, name : 'Pineapple', desc : 'Delicious Pineapple', price : '500', imageSrc : F2},
-]
+];
+
+export const categories = [
+    {
+        id : 1,
+        name : "Fruits",
+        urlParamName : "fruits", 
+    },
+    {
+        id : 2,
+        name : "Cereals",
+        urlParamName : "cereals",
+    },
+    {
+        id : 3,
+        name : "Spices",
+        urlParamName : "spices",
+    },
+    {
+        id : 4,
+        name : "Vegetables",
+        urlParamName : "vegetables",
+    },
+    {
+        id : 5,
+        name : "Seeds",
+        urlParamName : "seeds",
+    },
+    {
+        id : 6,
+        name : "Pulses",
+        urlParamName : "pulses",
+    },
+    // {
+    //     id : 7,
+    //     name : "Honey",
+    //     urlParamName : "honey",
+    // },
+    {
+        id : 7,
+        name : "Dry Fruits & Nuts",
+        urlParamName : "nuts",
+    },
+];
+
