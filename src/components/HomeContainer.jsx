af978029ecd9491632641039c804ef28ca58917c
Mousty Mainer <moustymainer@gmail.com>
2023-02-04 11:25:12 +0100

    Update HomeContainer.jsx

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -49,7 +49,7 @@ const HomeContainer = () => {
           alt="hero-bg"
         />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-">
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
           {heroData &&
             heroData.map((n) => (
               <div
