74ecfa0de44239071a9ad2e32a3f15402c52c0ff
Mousty Mainer <moustymainer@gmail.com>
2023-02-04 11:06:56 +0100

    home design change static = 3img

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-import F1 from '../img/f1.png'
+// import F1 from '../img/f1.png'
 import F9 from '../img/f9.png';
 import F10 from '../img/f10.png';
 import F2 from '../img/f2.png';
@@ -6,7 +6,7 @@ import F2 from '../img/f2.png';
 
 
 export const heroData = [
-    {id : 1, name : 'Fresh Strawberries', desc : 'All sweet berries', price : '1,500', imageSrc : F1},    
+    // {id : 1, name : 'Fresh Strawberries', desc : 'All sweet berries', price : '1,500', imageSrc : F1},    
     {id : 2, name : 'Fresh Banana', desc : 'Banana & Plantain', price : '1,000', imageSrc : F9},
     {id : 3, name : 'Watermelon', desc : 'Sweet watermelon', price : '300', imageSrc : F10},
     {id : 4, name : 'Pineapple', desc : 'Delicious Pineapple', price : '500', imageSrc : F2},
