cd3bfa1acf62971c313df7757a509025ccee4ef0
mainerprenuer <moustymainer@gmail.com>
2023-02-28 09:37:33 +0100

    added not data not available img

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -12,7 +12,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
     <div 
     ref={rowContainer}
       className={`w-full flex items-center gap-3 my-12 scroll-smooth ${ flag ? "overflow-x-scroll scrollbar-none" : "overflow-x-hidden flex-wrap justify-center"}`}>
-      {data ? data.map((item) => (
+      {data && data.length > 0 ? (data.map((item) => (
          <div key={item?.id} className='w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px] bg-cardOverlay rounded-lg py-2 px-4 my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'>
          <div className='w-full flex items-center justify-between'>
           <motion.div className='w-40 -mt-8 drop-shadow-2xl' whileHover={{scale : 1.2}}>
@@ -36,10 +36,12 @@ const RowContainer = ({ flag, data, scrollValue }) => {
            </div>
          </div>
        </div>
-      )) : <div className='w-full flex items-center justify-center'>
+      ))
+      ) : (<div className='w-full flex flex-col items-center justify-center'>
               <img src={NotFound} className='h-420' />
-              <p>Items Not Available</p>
-          </div>}
+              <p className='text-xl text-headingColor font-semibold my-2'>Items Not Available</p>
+          </div>
+          )}
     </div>
   )
 }
