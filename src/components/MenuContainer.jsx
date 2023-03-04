0c2cfe27646aaa17c237f08d6b373387a6cc46bc
mainerprenuer <moustymainer@gmail.com>
2023-03-04 09:50:38 +0100

    const filter on veg

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -7,19 +7,19 @@ import { useStateValue } from '../context/StateProvider'
 
 const MenuContainer = () => { 
 
-    const [filter, setFilter] = useState("Vegetables");
+    const [filter, setFilter] = useState("vegetables");
 
     const [{farmItems}, dispatch] = useStateValue ();
 
   return (
-    <section className='w-full my-6' id='menu'>
+    <section className='w-full h-full my-6' id='menu'>
         <div className='w-full flex flex-col items-center justify-center'>
         <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-20 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
             Our Farm Products
         </p>
 
         <div className='w-full flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none'>
-                {categories && categories.map(category => (
+                {categories && categories.map((category) => (
                     <motion.div whileTap={{scale : 0.75}}
                      key={category.id} className={`group ${filter === category.urlParamName ? 'bg-cartNumBg' : 'bg-card'} w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg`}
                     onClick={() => setFilter(category.urlParamName)}
@@ -33,7 +33,7 @@ const MenuContainer = () => {
             </div>
 
             <div className='w-full'>
-                <RowContainer flag={false} data={farmItems?.filter((n) => n.category === filter)}/>
+                <RowContainer flag={false} data={farmItems?.filter((n) => n.category == filter)}/>
             </div>
         </div>
     </section> 
